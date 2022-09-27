a3e2ac5a429c6cf72a7609fc70961e79dfdbb1d4
felistus-kavuu <81955754+felistus-kavuu@users.noreply.github.com>
2022-09-27 07:37:37 +0300

    Create divisiblesumpairs.hackerrank.py

---
+++ divisiblesumpairs.hackerrank.py
@@ -0,0 +1,60 @@
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+
+
+
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Solution 1:
+  def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    count=0
+    divisor=0
+    
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if (ar[i]+ar[j])%k==0:
+                count+=1
+    
+    
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+    
+    Solution 2:
+      

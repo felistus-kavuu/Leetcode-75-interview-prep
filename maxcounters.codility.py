30af15420cdc29a2c5d619d0ae260610cbe87a8f
felistus-kavuu <81955754+felistus-kavuu@users.noreply.github.com>
2022-10-21 16:35:05 +0300

    Create maxcounters.codility.py

---
+++ maxcounters.codility.py
@@ -0,0 +1,79 @@
+You are given N counters, initially set to 0, and you have two possible operations on them:
+
+increase(X) − counter X is increased by 1,
+max counter − all counters are set to the maximum value of any counter.
+A non-empty array A of M integers is given. This array represents consecutive operations:
+
+if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+if A[K] = N + 1 then operation K is max counter.
+For example, given integer N = 5 and array A such that:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the values of the counters after each consecutive operation will be:
+
+    (0, 0, 1, 0, 0)
+    (0, 0, 1, 1, 0)
+    (0, 0, 1, 2, 0)
+    (2, 2, 2, 2, 2)
+    (3, 2, 2, 2, 2)
+    (3, 2, 2, 3, 2)
+    (3, 2, 2, 4, 2)
+The goal is to calculate the value of every counter after all operations.
+
+Write a function:
+
+def solution(N, A)
+
+that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the function should return [3, 2, 2, 4, 2], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and M are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..N + 1].
+
+************************************************************************************
+
+def solution(N, A):
+    # write your code in Python 3.8.10
+    RESP = [0] * N
+    MAX_OPERATION = N + 1
+    current_max = 0
+    current_min = 0
+    for operation in A:
+        if operation != MAX_OPERATION:
+            if RESP[operation-1] <= current_min:
+                RESP[operation-1] = current_min + 1
+            else:
+                RESP[operation-1] += 1
+
+            if RESP[operation-1] > current_max:
+                current_max = RESP[operation-1]
+        else:
+            if current_min == current_max:
+                current_min += 1
+            else:
+                current_min = current_max
+
+    for i, val in enumerate(RESP):
+        if val < current_min:
+            RESP[i] = current_min
+    return RESP
